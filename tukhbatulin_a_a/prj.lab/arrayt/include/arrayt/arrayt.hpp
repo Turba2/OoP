fd95166260e383e210a9c65d037033df9a03f2d5
Turba2 <m2209147@edu.misis.ru>
2023-06-23 18:11:04 +0300

    update

--- tukhbatulin_a_a/prj.lab/arrayt/include/arrayt/arrayt.hpp
+++ tukhbatulin_a_a/prj.lab/arrayt/include/arrayt/arrayt.hpp
@@ -1,144 +1,29 @@
 #pragma once
 
-#ifndef ARRAYD_HPP
-#define ARRAYD_HPP
+#ifndef ARRAYT_HPP
 
-#include <iosfwd>
 #include <cstdint>
+#include <iosfwd>
 
-template <typename T = double>
-
-class ArrayD{
+template <typename T>
+class ArrayT{
 public:
+    ArrayT() noexcept;
+    explicit ArrayT(const ptrdiff_t size, const T num = T());
+    ~ArrayT();
+    ArrayT(const ArrayT&);
+    ArrayT& operator=(const ArrayT&);
+    void reserve(ptrdiff_t size) noexcept;
 
-    ArrayD() noexcept{
-        ssize_ = 0;
-        capacity_ = 1;
-        data_ = reinterpret_cast<T*>(new int8_t[sizeof(T)]);
-        //data_ = new T[capacity_];
-    }
-
-    ArrayD(ptrdiff_t size){
-        if(size < 0){
-            throw std::invalid_argument("bad size");
-        }
-        data_ = reinterpret_cast<T*>(new int8_t[2 * size * sizeof(T)]);
-        ssize_ = size;
-        capacity_ = size;
-        ptrdiff_t i = 0;
-        try{
-            for(; i < ssize_; ++i){
-                new (data_ + i) T(); // placement new
-            }
-        } catch(...){
-            for(ptrdiff_t j = 0; j < i; ++j){
-                (data_ + j)->~T();
-            }
-            delete[] reinterpret_cast<char*>(data_);
-            throw;
-        }
-    }
-    ArrayD(const ArrayD& other) noexcept{
-        T* data_ = reinterpret_cast<T*>(new int8_t[other.ssize_ * sizeof(T)]);
-        ssize_ = other.ssize_;
-        capacity_ = other.capacity_;
-        ptrdiff_t i = 0;
-        try{
-            for(; i < ssize_; ++i){
-                new (data_ + i) T(other.data_[i]);
-            }
-        } catch(...){
-            for(ptrdiff_t j = 0; j < i; ++j){
-                (data_ + j)->~T();
-            }
-            delete[] reinterpret_cast<char*>(data_);
-            throw;
-        }
-    }
-
-    ArrayD& operator=(const ArrayD& rhs) noexcept{
-        capacity_ = rhs.capacity_;
-        ptrdiff_t i = 0;
-        for(ptrdiff_t i = 0; i < ssize_; ++i){
-            (data_ + i)->~T();
-        }
-        reserve(capacity_);
-        for(i = 0; i < rhs.ssize_; ++i){
-            new (data_ + i) T(rhs.data_[i]);
-        }
-        ssize_ = rhs.ssize_;
-        return *this;
-    }
-
-    void reserve(ptrdiff_t size){
-        if (size < 0){
-            throw std::invalid_argument("bad size");
-        }
-        if(size <= capacity_) return;
-        T* newarr = reinterpret_cast<T*>(new int8_t[size * sizeof(T)]);
-        ptrdiff_t i = 0;
-        try{
-            for(; i < ssize_; ++i){
-                new (newarr + i) T(data_[i]); // placement new
-            }
-        } catch(...){
-            for(ptrdiff_t j = 0; j < i; ++j){
-                (newarr + j)->~T();
-            }
-            delete[] reinterpret_cast<char*>(newarr);
-            throw;
-        }
-        for(ptrdiff_t i = 0; i < ssize_; ++i){
-            (data_ + i)->~T();
-        }
-        delete[] reinterpret_cast<char*>(data_);
-        data_ = newarr;
-        capacity_ = size;
-    }
-
-    void resize(const ptrdiff_t size, const T& value = T()){
-        if (size < 0){
-            throw std::invalid_argument("bad size");
-        }
-        if(size > capacity_) reserve(size);
-        for(ptrdiff_t i = ssize_; i < size; ++i){
-            new (data_ + i) T(value);
-        }
-        if(size < ssize_){
-            ssize_ = size;
-        }
-    }
-
-    void push_back(const T& value) noexcept{
-        if(capacity_ == ssize_) reserve(2 * capacity_);
-        new(data_ + ssize_) T(value);
-        ssize_ += 1;
-    } 
-
-    void pop_back(){
-        if(ssize_ <= 0){
-            throw std::out_of_range("Dynamic ArrayD is empty");
-        }
-        ssize_ -= 1;
-        (data_ + ssize_)->~T();
-    }
-
-
-    T& operator[](ptrdiff_t index){
-        if(index < 0 || index >= ssize_){
-            throw std::out_of_range("Index out of range");
-        }
-        return data_[index];
-    }
-
-    const T& operator[](ptrdiff_t index) const{
-        if(index < 0 || index >= ssize_){
-            throw std::out_of_range("Index out of range");
-        }
-        return data_[index];
-    }
-    
-    bool operator==(const ArrayD& rhs) noexcept{
+    T& operator[](const ptrdiff_t i);
+    const T& operator[](const ptrdiff_t i) const;
+    ptrdiff_t ssize() const noexcept;
+    void resize(const ptrdiff_t new_size);
+    void insert(const ptrdiff_t ind, const T value = T());
+    void remove(const ptrdiff_t i);
+    void push_back(double numb) noexcept;
+    void pop_back();
+    bool operator==(const ArrayT& rhs) noexcept{
         if(this->ssize() != rhs.ssize()){
             return 0;
         }
@@ -152,79 +37,150 @@ public:
         return fl;
     }
 
-    ptrdiff_t ssize() const noexcept{
-        return ssize_;
-    }
-
-    void insert(const ptrdiff_t ind, const T& value){
-        if(ind > ssize_ || ind < 0){
-            throw std::out_of_range("Index out of range");
-        }
-        if(ssize_ == capacity_) reserve(2 * capacity_);
-        T* newarr = reinterpret_cast<T*>(new int8_t[(ssize_ + 3) * sizeof(T)]);
-        ptrdiff_t i = 0;
-        try{
-            for(; i < ind; ++i){
-                new (newarr + i) T(data_[i]);
-            }
-            new (newarr + ind) T(value);
-            for(i = ind; i < ssize_; ++i){
-                new (newarr + i + 1) T(data_[i]);
-            }
-        }
-        catch(...){
-            for(i = 0; i < ssize_ + 3; ++i){
-                (newarr + i)->~T();
-            }
-        }
-        for(i = 0; i < ssize_; ++i){
-            (data_ + i)->~T();
-        }
-        for(i = 0; i < ssize_ + 1; ++i){
-            new (data_ + i) T(newarr[i]);
-        }
-        for(i = 0; i < ssize_ + 1; ++i){
-            (newarr + i)->~T();
-        }
-        delete[] reinterpret_cast<char*>(newarr); 
-        ssize_ += 1;
-    }
-
-    void remove(const ptrdiff_t ind){
-        if (ind >= ssize_ || ind < 0){
-            throw std::out_of_range("Index out of range");
-        }
-        T* newarr = reinterpret_cast<T*>(new int8_t[ssize_ * sizeof(T)]);
-        ptrdiff_t i = 0;
-        try{
-            for(; i < ssize_; ++i){
-                new (newarr + i) T(data_[i]);
-            }
-        }
-        catch(...){
-            for(i = 0; i < ssize_ - ind + 1; ++i){
-                (newarr + i)->~T();
-            }
-            throw;
-        }
-        i = ind;
-        (data_ + ind)->~T();
-        for(; i < ssize_ - 1; ++i){
-            (data_ + i)->~T();
-            new(data_ + i) T(newarr[i + 1]);
-        }
-        for(i = 0; i < ssize_; ++i){
-            (newarr + i)->~T();
-        }
-        delete[] reinterpret_cast<char*>(newarr); 
-        ssize_ -= 1;
-    }
-
-
 private:
     T* data_;
-    ptrdiff_t ssize_;
     ptrdiff_t capacity_;
+    ptrdiff_t size_;
 };
 
-#endif
+
+
+template <typename T> ArrayT<T>::ArrayT() noexcept{
+    size_ = 0;
+    capacity_ = 0;
+    data_ = nullptr;
+}
+
+template<class T> ArrayT<T>::ArrayT(ptrdiff_t size, T num){
+    if (size < 0) {
+        throw std::invalid_argument("Invalid argument");
+    }
+    capacity_ = size;
+    if(size == 0){
+        capacity_ = 0;
+    }
+    size_ = size;
+    data_ = new T[size];
+    std::fill(data_, data_ + size, num);
+}
+
+template<class T> ArrayT<T>::ArrayT(const ArrayT<T> &temp){
+    data_ = new T[temp.capacity_];
+    std::uninitialized_copy(temp.data_, temp.data_ + temp.size_, data_);
+    size_ = temp.size_;
+    capacity_ = temp.capacity_;
+}
+
+
+template<class T> ArrayT<T>::~ArrayT(){
+    delete[] data_;
+}
+
+template<class T> ArrayT<T> &ArrayT<T>::operator=(const ArrayT<T> &rhs){
+    if (this == &rhs) {
+        return *this;
+    }
+    delete[] reinterpret_cast<char*>(data_);
+    size_ = rhs.size_;
+    capacity_ = rhs.capacity_;
+    data_ = new T[size_];
+    std::uninitialized_copy(rhs.data_, rhs.data_ + size_, data_);
+    return *this;
+}
+
+template<class T> ptrdiff_t ArrayT<T>::ssize() const noexcept{
+    return size_;
+}
+
+
+template<class T> T &ArrayT<T>::operator[](ptrdiff_t index){
+    if (index < 0 || index >= size_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[index];
+}
+
+template<class T> const T &ArrayT<T>::operator[](ptrdiff_t index) const{
+    if (index < 0 || index >= size_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[index];
+}
+
+template<class T> void ArrayT<T>::reserve(ptrdiff_t size) noexcept{
+    if(size < capacity_){
+        return;
+    }
+    double* newarr = new double[capacity_];
+    std::uninitialized_copy(data_, data_ + capacity_, newarr);
+    delete[] reinterpret_cast<char*>(data_);
+    data_ = new T[size];
+    std::uninitialized_copy(newarr, newarr + capacity_, data_);
+    std::fill(data_ + size_, data_ + size, 0);
+    capacity_ = size;
+    delete[] reinterpret_cast<char*>(newarr);   
+}
+
+template<class T> void ArrayT<T>::resize(ptrdiff_t new_size){
+    if (new_size <= 0){
+        throw std::invalid_argument("Invalid argument");
+    }
+    if(new_size > capacity_){
+        reserve(new_size);
+    }
+    if (new_size > size_){
+        std::fill(data_ + size_, data_ + new_size, 0);
+    }
+    size_ = new_size;
+    std::fill(data_ + size_, data_ + capacity_, 0);
+}
+
+
+
+template<class T> void ArrayT<T>::push_back(double numb) noexcept{
+    if(size_ == capacity_){
+        reserve(2 * capacity_);
+    }
+    data_[size_] = numb;
+    size_ += 1;
+}
+
+template<class T> void ArrayT<T>::pop_back(){
+    if(size_ == 0){
+        throw std::out_of_range("List is empty");
+    }
+    size_ -= 1;
+
+}
+
+template<class T> void ArrayT<T>::insert(ptrdiff_t ind, T value){
+    if(ind > size_ || ind < 0){
+            throw std::out_of_range("Index out of range");
+    }
+    if (capacity_ == 0){reserve(2);}
+    else if(size_ == capacity_) reserve(2 * capacity_);
+
+    double* newarr = reinterpret_cast<double*>(new int8_t[(size_ + 3) * sizeof(double)]);
+    ptrdiff_t i = 0;
+    for(; i < ind; ++i){
+        newarr[i] = data_[i];
+    }
+    newarr[ind] = value;
+    for(i = ind; i < size_; ++i){
+        newarr[i + 1] = data_[i];
+    }
+    size_ += 1;
+    std::uninitialized_copy(newarr, newarr + size_, data_);
+    delete[] reinterpret_cast<char*>(newarr);
+}
+
+template<class T> void ArrayT<T>::remove(ptrdiff_t ind){
+    if (ind < 0 || ind >= size_) throw std::out_of_range("out of range");
+    ptrdiff_t i = ind + 1;
+    for(; i < size_; ++i){
+        data_[i - 1] = data_[i];
+    }
+    size_ -= 1;
+}
+
+#endif // ARRAYT_HPP
